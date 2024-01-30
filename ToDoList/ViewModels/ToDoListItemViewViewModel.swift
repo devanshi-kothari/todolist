46e050a9cf36d9bba805bc060d432dc7b4880519
Devanshi Kothari <dkot26@seas.upenn.edu>
2024-01-29 19:33:13 -0500

    log out

--- ToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,9 +5,27 @@
 //  Created by Devanshi Kothari on 1/12/24.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 //view model for a single todo list item view (each row in items list)
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
 }
